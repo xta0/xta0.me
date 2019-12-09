22f095d2b99055748bd492446ad044a237e9b3f7
Tao Xu <taox@fb.com>
2019-12-08 19:42:54 -0800

    Update(auto commit)

--- assets/scripts/helpers.py
+++ assets/scripts/helpers.py
@@ -2,13 +2,12 @@ import os
 import string
 import argparse
 
-def change_year(path, keyword, new_year=""):
+def change_year(path, keyword, old_year, new_year):
     for file in os.listdir(path):
         results = []
         if keyword in file:
             print(f'found "{file}"')
-            year=file[:5]
-            new_name = file.replace(year,new_year)
+            new_name = file.replace(old_year,new_year)
             print(f'change to {new_name}')
             os.rename(os.path.join(path,file), os.path.join(path,new_name))
     print('Done.')
@@ -18,9 +17,11 @@ if __name__ == '__main__':
         description='Rename Files')
     parser.add_argument('path', metavar='PATH', type=str,help='Input file directory')
     parser.add_argument('keyword', metavar='PATH', type=str,help='File names that contains the keyword')
+    parser.add_argument('old_year', metavar='old year', type=str, help='Old year')
     parser.add_argument('new_year', metavar='new year', type=str, help='New year')
     input_dir = parser.parse_args().path
-    new_years = parser.parse_args().new_year
+    old_year = parser.parse_args().old_year
+    new_year = parser.parse_args().new_year
     keyword   = parser.parse_args().keyword
-    change_year(input_dir, keyword,new_years)
+    change_year(input_dir, keyword, old_year, new_year)
     
